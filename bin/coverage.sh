5f5b22b419344e05f7f21c68cc133048e76d8e05
zsummer <yawei.zhang@foxmail.com>
2022-09-05 16:23:29 +0800

    coverage shell

---
+++ bin/coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+task_list="base_env_test base_socket_test base_session_multi_bind_test base_session_connect_test base_session_flow_test"
+
+for task in ${task_list};
+do
+  ./$task
+  if [ $? -ne 0 ]; then 
+    echo "$task has error"
+  else
+    echo "$task test success"
+  fi
+  echo ""
+  echo ""
+done
+
+echo "coverage.sh finish"
---
+++ bin/make.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ..
+bash make.sh
+
