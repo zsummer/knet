114282f9edbe09fabfd4f96037b403195f210f16
zsummer <yawei.zhang@foxmail.com>
2022-09-05 16:09:33 +0800

    multi bind test

--- tests/base_session_connect_test/main.cpp
+++ tests/base_session_connect_test/main.cpp
@@ -12,39 +12,11 @@ if (!(x))  \
 }
 
 
-s32 test_socket_bind()
-{
-	KNetConfigs mc;
-	mc.emplace_back(KNetConfig{ "127.0.0.1", 19870, "", 0});
-	mc.emplace_back(KNetConfig{ "127.0.0.2", 19870, "", 0 });
-	KNetController controller;
-	s32 ret = controller.start_server(mc);
-	KNetAssert(ret == 0, "");
-
-	KNetSocket s1(1);
-	KNetSocket s2(2);
-	ret = s1.init("127.0.0.1", 19870, "127.0.0.1", 8080);
-	KNetAssert(ret != 0, "");
-	ret = s2.init("127.0.0.2", 19870, "127.0.0.1", 8080);
-	KNetAssert(ret != 0, "");
-
-	controller.destroy();
-	ret = s1.init("127.0.0.1", 19870, "127.0.0.1", 8080);
-	KNetAssert(ret == 0, "");
-	ret = s2.init("127.0.0.2", 19870, "127.0.0.1", 8080);
-	KNetAssert(ret == 0, "");
-	s1.destroy();
-	s2.destroy();
-	KNetAssert(KNetEnv::error_count() == 0, "");
-	return 0;
-}
 
 int main()
 {
 	FNLog::FastStartDebugLogger();
 	LogInfo() << "start up";
-	KNetAssert(test_socket_bind() == 0, "");
-	//KNetAssert(test_socket_bind2() == 0, "");
 
 	KNetEnv::clean_count();
 	KNetEnv::error_count() = 0;
---
+++ tests/base_session_multi_bind_test/main.cpp
@@ -0,0 +1,50 @@
+#include "knet_base.h"
+#include "knet_socket.h"
+#include "knet_controller.h"
+KNetEnv env_init_;
+
+
+#define KNetAssert(x, desc)  \
+if (!(x))  \
+{   \
+	LogError() << desc << "has error.";  \
+	return -1;\
+}
+
+KNetController controller;
+
+s32 test_session_bind()
+{
+	KNetConfigs mc;
+	mc.emplace_back(KNetConfig{ "127.0.0.1", 19870, "", 0});
+	mc.emplace_back(KNetConfig{ "127.0.0.2", 19870, "", 0 });
+	
+	s32 ret = controller.start_server(mc);
+	KNetAssert(ret == 0, "");
+
+	KNetSocket s1(1);
+	KNetSocket s2(2);
+	ret = s1.init("127.0.0.1", 19870, "127.0.0.1", 8080);
+	KNetAssert(ret != 0, "");
+	ret = s2.init("127.0.0.2", 19870, "127.0.0.1", 8080);
+	KNetAssert(ret != 0, "");
+
+	controller.destroy();
+	ret = s1.init("127.0.0.1", 19870, "127.0.0.1", 8080);
+	KNetAssert(ret == 0, "");
+	ret = s2.init("127.0.0.2", 19870, "127.0.0.1", 8080);
+	KNetAssert(ret == 0, "");
+	s1.destroy();
+	s2.destroy();
+	KNetAssert(KNetEnv::error_count() == 0, "");
+	return 0;
+}
+
+int main()
+{
+	FNLog::FastStartDebugLogger();
+	LogInfo() << "start up";
+	KNetAssert(test_session_bind() == 0, "");
+	return 0;
+}
+
